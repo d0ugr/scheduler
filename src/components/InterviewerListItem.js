ff48f232ba6b2790e6c6cf5a685d95ce1462bd42
d0ugr <doug@escm.ml>
2020-04-14 12:05:00 -0400

    Add InterviewerListItem and stories

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React      from "react";
+import classNames from "classnames";
+
+import "./InterviewerListItem.scss";
+
+
+
+export default function InterviewerListItem(props) {
+
+  const interviewerListItemClasses = classNames(
+    "interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+  const interviewerListItemImageClasses = classNames(
+    "interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected
+  });
+
+  return (
+    <li
+      className={interviewerListItemClasses}
+      onClick={() => props.setInterviewer(props.name)}
+    >
+      <img
+        className={interviewerListItemImageClasses}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+
+}
