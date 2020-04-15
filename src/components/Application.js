1fb37a7ce810012f9cf73184faacf8d89f8cd45d
d0ugr <doug@escm.ml>
2020-04-15 11:05:19 -0400

    Update Application to use Axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-import DayList from "./DayList";
+import DayList     from "./DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
 
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+    interview: {
+      student: "Duuuuuuuuuuuuuuuuuuuuuuuuuuuuude",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
   },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4:20pm",
+    interview: {
+      student: "Towlie",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  }
 ];
 
 
@@ -29,6 +60,21 @@ const days = [
 export default function Application(_props) {
 
   const [ selectedDay, setDay ] = useState("Monday");
+  const [ days, setDays ]       = useState([]);
+
+  const appointmentItems = appointments.map((appointment, _index) =>
+    <Appointment
+      key={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+    />
+  );
+
+  useEffect(() => {
+    Axios.get("/api/days")
+    .then((res) => setDays(res.data))
+    .catch((err) => console.log(err));
+  }, []);
 
   return (
     <main className="layout">
@@ -53,7 +99,7 @@ export default function Application(_props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentItems}
       </section>
     </main>
   );
