0d7e2b4aded0c67fcdbe483647c26de8ce9323b3
d0ugr <doug@escm.ml>
2020-04-15 16:09:20 -0400

    Update to use API server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,80 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Axios from "axios";
 
 import DayList     from "./DayList";
 import Appointment from "components/Appointment";
+import * as select from "helpers/selectors";
 
 import "components/Application.scss";
 
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Duuuuuuuuuuuuuuuuuuuuuuuuuuuuude",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4:20pm",
-    interview: {
-      student: "Towlie",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  }
-];
-
-
-
 export default function Application(_props) {
 
-  const [ selectedDay, setDay ] = useState("Monday");
-  const [ days, setDays ]       = useState([]);
+  const defaultState = {
+    selectedDay:      null,
+    days:             null,
+    appointments:     null,
+    appointmentItems: null
+  };
 
-  const appointmentItems = appointments.map((appointment, _index) =>
-    <Appointment
-      key={appointment.id}
-      time={appointment.time}
-      interview={appointment.interview}
-    />
-  );
+  const [ state, setState ] = useState(defaultState);
+
+  const updateState = useCallback((data) => {
+    setState((prev) =>
+      (data
+        ? {
+          ...prev,
+          ...(data.name && data.value ? { [data.name]: data.value } : data)
+        }
+        : defaultState
+      )
+    );
+  }, [ defaultState ]);
 
   useEffect(() => {
-    Axios.get("/api/days")
-    .then((res) => setDays(res.data))
+    console.log("useEffect: Initial page load");
+    Promise.all([
+      Axios.get("/api/days"),
+      Axios.get("/api/appointments")
+    ])
+    .then((res) => {
+      updateState({
+        days:         res[0].data,
+        appointments: res[1].data
+      })
+    })
     .catch((err) => console.log(err));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    console.log("useEffect: state.selectedDay, state.appointments");
+    updateState({
+      appointmentItems: select.getAppointmentsForDay(state, state.selectedDay).map((appointment, _index) =>
+        <Appointment
+          key={appointment.id}
+          time={appointment.time}
+          interview={appointment.interview}
+        />
+      )})
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ state.selectedDay, state.appointments ]);
 
   return (
     <main className="layout">
@@ -87,9 +72,9 @@ export default function Application(_props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            selectedDay={selectedDay}
-            setDay={day => setDay(day)}
+            days={state.days}
+            selectedDay={state.selectedDay}
+            setDay={day => updateState({ selectedDay: day })}
           />
         </nav>
         <img
@@ -99,7 +84,7 @@ export default function Application(_props) {
         />
       </section>
       <section className="schedule">
-        {appointmentItems}
+        {state.appointmentItems}
       </section>
     </main>
   );
