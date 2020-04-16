57be1f535abfdab08350824a494419357d9b29fc
d0ugr <doug@escm.ml>
2020-04-16 13:45:48 -0400

    Refactor selectors into non-stupid code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,32 +3,41 @@
 
 // getAppointmentsForDay returns an array of appointment objects for the given day.
 
-export const getAppointmentsForDay = (state, dayId) =>
-  ( ( ( (state && state.days) || []
-      ).find((stateDay) => stateDay.id === dayId) || {}
-    ).appointments || []
-  ).map((appointmentId) => state.appointments[appointmentId])
-;
+export const getAppointmentsForDay = (state, dayId) => {
+  try {
+    return state.days
+      .find((stateDay) => stateDay.id === dayId)
+      .appointments.map((appointmentId) => state.appointments[appointmentId]);
+  } catch (err) {
+    return [];
+  }
+};
 
 // getInterviewersForDay returns an array of interviewer objects for the given day.
 
-export const getInterviewersForDay = (state, dayId) =>
-  ( ( ( (state && state.days) || []
-      ).find((stateDay) => stateDay.id === dayId) || {}
-    ).interviewers || []
-  ).map((interviewerId) => state.interviewers[interviewerId])
-;
+export const getInterviewersForDay = (state, dayId) => {
+  try {
+    return state.days
+      .find((stateDay) => stateDay.id === dayId)
+      .interviewers.map((interviewerId) => state.interviewers[interviewerId]);
+  } catch (err) {
+    return [];
+  }
+};
 
 // getInterview returns a full interview object for an interview
 //    object coming from the appointments API.
 
-export const getInterview = (state, interview) =>
-  ( state && state.interviewers && interview && interview.interviewer && {
-      ...interview,
+export const getInterview = (state, interview) => {
+  try {
+    return {
+      student:     interview.student,
       interviewer: { ...state.interviewers[interview.interviewer] }
-    }
-  )
-;
+    };
+  } catch (err) {
+    return null;
+  }
+};
 
 
 
