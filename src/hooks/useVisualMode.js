b6c59740ddb6fdf0099e143426fe4aeb023f99c1
d0ugr <doug@escm.ml>
2020-04-15 22:41:11 -0400

    Add useVisualMode back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,16 +3,33 @@ import { useState } from "react";
 
 
 export default function useVisualMode(initial) {
-  const [ mode, setMode ] = useState(initial);
+
+  const [ mode,    setMode    ] = useState(initial);
+  const [ history, setHistory ] = useState([ initial ]);
+  // console.log("useVisualMode:", history);
 
   function transition(newMode) {
     setMode(newMode);
+    history.push(newMode);
+    setHistory([ ...history ]);
+    // console.log("transition:", history);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+      setHistory([ ...history ]);
+      // console.log("back:", history);
+    }
   }
 
   return {
     mode,
-    transition
+    transition,
+    back
   };
+
 }
 
 
