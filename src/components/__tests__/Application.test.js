f214c38a26fd442a5b9b0b31247e862df5982295
d0ugr <doug@escm.ml>
2020-04-21 02:15:49 -0400

    Switch to async/await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,13 +8,11 @@ afterEach(cleanup);
 
 
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Archie Cohen")).toBeInTheDocument();
-    });
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Archie Cohen")).toBeInTheDocument();
 });
 
 
