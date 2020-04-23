3efc1246add0e0897d9e8b755c24b42f16e04794
d0ugr <doug@escm.ml>
2020-04-23 16:02:59 -0400

    Return to original importing style

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,38 +1,42 @@
 import React from "react";
-// import { render, cleanup, prettyDOM, fireEvent, waitForElement, getByText } from "@testing-library/react";
-import * as rtl from "@testing-library/react";
+import {
+  render, cleanup, prettyDOM,
+  fireEvent, waitForElement, getAllByTestId,
+  getByText, getByAltText, getByPlaceholderText,
+  queryByText
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
-afterEach(rtl.cleanup);
+afterEach(cleanup);
 
 
 
 describe("Application", () => {
 
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-    const ar = rtl.render(<Application />);
-    await rtl.waitForElement(() => ar.getByText("Monday"));
-    rtl.fireEvent.click(ar.getByText("Tuesday"));
+    const ar = render(<Application />);
+    await waitForElement(() => ar.getByText("Monday"));
+    fireEvent.click(ar.getByText("Tuesday"));
     expect(ar.getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const ar = rtl.render(<Application />);
-    await rtl.waitForElement(() => rtl.getByText(ar.container, "Archie Cohen"));
-    const appointment = rtl.getAllByTestId(ar.container, "appointment")[0];
-    rtl.fireEvent.click(rtl.getByAltText(appointment, "Add"));
-    rtl.fireEvent.change(rtl.getByPlaceholderText(appointment, /enter student name/i), {
+    const ar = render(<Application />);
+    await waitForElement(() => getByText(ar.container, "Archie Cohen"));
+    const appointment = getAllByTestId(ar.container, "appointment")[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-    rtl.fireEvent.click(rtl.getByAltText(appointment, "Sylvia Palmer"));
-    rtl.fireEvent.click(rtl.getByText(appointment, "Save"));
-    expect(rtl.getByText(appointment, "Saving...")).toBeInTheDocument();
-    await rtl.waitForElement(() => rtl.getByText(appointment, "Lydia Miller-Jones"));
-    const day = rtl.getAllByTestId(ar.container, "day").find((day) =>
-      rtl.queryByText(day, "Monday")
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    const day = getAllByTestId(ar.container, "day").find((day) =>
+      queryByText(day, "Monday")
     );
-    expect(rtl.getByText(day, /no spots remaining/i)).toBeInTheDocument();
+    expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
   });
 
 });
