990967e7e776fe5d26ba5f7010dda6be9c1df48d
d0ugr <doug@escm.ml>
2020-04-23 16:04:39 -0400

    Add cancel interview test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -39,6 +39,19 @@ describe("Application", () => {
     expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
   });
 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+    const ar = render(<Application />);
+    await waitForElement(() => getByText(ar.container, "Archie Cohen"));
+    // 3. Click the Delete button on the appointment.
+    // 4. Check that the confirmation prompt is shown.
+    // 5. Click the Confirm button.
+    // 6. Check that the element with the text Deleting... is displayed.
+    // 7. Wait until the empty appointment with the Add button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
+  });
+
 });
 
 
