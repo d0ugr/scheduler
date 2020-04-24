b8e1df6d67624cd820c94fbf35b91fd431604f29
d0ugr <doug@escm.ml>
2020-04-24 17:52:27 -0400

    Add cancel interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -10,9 +10,7 @@ describe("Appointments", () => {
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+    cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
@@ -21,9 +19,7 @@ describe("Appointments", () => {
   });
 
   it("should edit an interview", () => {
-    cy.get("[alt=Edit]")
-      .first()
-      .click({ force: true });
+    cy.get("[alt=Edit]").first().click({ force: true });
     cy.get("[data-testid=student-name-input]")
       //.clear()  // I added focus + select all so this isn't necessary
       .type("Lydia Miller-Jones");
@@ -31,6 +27,15 @@ describe("Appointments", () => {
     cy.contains("Save").click();
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting...").should("exist");
+    cy.contains("Deleting...").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 
 });
